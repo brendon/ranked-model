e3a9b7fc242cc99b75dd1509213f2e6a321e26c3
Andrew Radev <andrey.radev@gmail.com>
2011-02-18 21:32:26 +0200

    More informative error messages
    Easier to understand what's going on when you've made a typo or
    something.

---
+++ spec/duck-model/wrong_ducks_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe WrongScopeDuck do
+
+  it "should raise an error because of an unknown scope" do
+
+    expect {
+      WrongScopeDuck.create(:name => 'Quocky', :pond => 'Shin')
+    }.to raise_error 'No scope called "non_existant_scope" found in model'
+
+  end
+
+end
+
+describe WrongFieldDuck do
+
+  it "should raise an error because of an unknown field" do
+
+    expect {
+      WrongFieldDuck.create(:name => 'Quicky', :pond => 'Shin')
+    }.to raise_error 'No field called "non_existant_field" found in model'
+
+  end
+
+end
