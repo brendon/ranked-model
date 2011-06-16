bc136ad67cebd164c457d5b4774fda88d476c697
Matthew Beale <matt.beale@madhatted.com>
2011-06-16 05:47:52 -0400

    More position tests for STI

--- spec/sti-model/element_spec.rb
+++ spec/sti-model/element_spec.rb
@@ -78,4 +78,45 @@ describe Element do
 
   end
 
+  describe "setting positions on STI classes" do
+
+    before {
+      @elements[:helium].update_attribute :combination_order_position, :first
+      @elements[:xenon].update_attribute :combination_order_position, :first
+      @elements[:argon].update_attribute :combination_order_position, :first
+
+      @elements[:chromium].update_attribute :combination_order_position, 1
+      @elements[:manganese].update_attribute :combination_order_position, 1
+      @elements[:manganese].update_attribute :combination_order_position, 0
+      @elements[:chromium].update_attribute :combination_order_position, 0
+      @elements[:manganese].update_attribute :combination_order_position, 0
+      @elements[:chromium].update_attribute :combination_order_position, 0
+    }
+
+    describe "NobleGas" do
+
+      subject { NobleGas.rank(:combination_order) }
+
+      its(:size) { should == 3 }
+      
+      its(:first) { should == @elements[:argon] }
+      
+      its(:last) { should == @elements[:helium] }
+
+    end
+
+    describe "TransitionMetal" do
+
+      subject { TransitionMetal.rank(:combination_order) }
+
+      its(:size) { should == 2 }
+      
+      its(:first) { should == @elements[:chromium] }
+      
+      its(:last) { should == @elements[:manganese] }
+
+    end
+
+  end
+
 end
