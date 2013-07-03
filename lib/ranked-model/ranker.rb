84d25d76b7e3fc7eed48a6e4365d4247dec4c98b
Sergey Martynov <martynovs@gmail.com>
2013-07-03 15:34:31 +0300

    include in finder query columns from 'with_same' option

--- lib/ranked-model/ranker.rb
+++ lib/ranked-model/ranker.rb
@@ -197,36 +197,29 @@ module RankedModel
       def finder
         @finder ||= begin
           _finder = instance_class
-          columns = [instance_class.arel_table[instance_class.primary_key], instance_class.arel_table[ranker.column]]
+          _table  = instance_class.arel_table
+          columns = [_table[instance_class.primary_key], _table[ranker.column]]
           if ranker.scope
             _finder = _finder.send ranker.scope
           end
           case ranker.with_same
             when Symbol
-              columns << instance_class.arel_table[ranker.with_same]
-              _finder = _finder.where \
-                instance_class.arel_table[ranker.with_same].eq(instance.attributes["#{ranker.with_same}"])
+              columns << _table[ranker.with_same]
+              _finder = _finder.where _table[ranker.with_same].eq(instance.attributes["#{ranker.with_same}"])
             when Array
-              ranker.with_same.each {|c| columns.push instance_class.arel_table[c] }
+              ranker.with_same.each {|c| columns << _table[c] }
               _finder = _finder.where(
                 ranker.with_same[1..-1].inject(
-                  instance_class.arel_table[ranker.with_same.first].eq(
-                    instance.attributes["#{ranker.with_same.first}"]
-                  )
+                  _table[ranker.with_same.first].eq(instance.attributes["#{ranker.with_same.first}"])
                 ) {|scoper, attr|
-                  scoper.and(
-                    instance_class.arel_table[attr].eq(
-                      instance.attributes["#{attr}"]
-                    )
-                  )
+                  scoper.and(_table[attr].eq(instance.attributes["#{attr}"]))
                 }
               )
           end
           if !new_record?
-            _finder = _finder.where \
-              instance_class.arel_table[instance_class.primary_key].not_eq(instance.id)
+            _finder = _finder.where _table[instance_class.primary_key].not_eq(instance.id)
           end
-          _finder.order(instance_class.arel_table[ranker.column].asc).select(columns)
+          _finder.order(_table[ranker.column].asc).select(columns)
         end
       end
 
